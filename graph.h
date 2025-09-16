fda4a2f1285c54249739cdc64c5012299c6cb984
kpatcha lelenda arnaud <112127285+Lelenda-Kpatcha@users.noreply.github.com>
2025-09-16 14:57:04 +0200

    create struct of the graph

---
+++ graph.h
@@ -0,0 +1,35 @@
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdbool.h>
+
+//Define a type for value of a vertex name
+
+typedef int vertex_name ;
+
+
+typedef struct neighbour_t {
+    vertex_name dest;
+    int weight;  
+    struct neighbour_t *next;
+} neighbour;
+
+//Define a type for a vertex 
+
+typedef struct vertex {
+    vertex_name name ;
+    neighbour *adj_list ;
+} vertex ;
+
+//Define a type for a  graph
+
+typedef struct graph_t {
+   bool oriented ;
+   bool weighted ;
+   int nb_vertex ;
+   vertex  *vectrices ;
+} graph ;
+
+
+#endif
